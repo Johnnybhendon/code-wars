d51aa6b18f7a5c4953f93fda0ea57c708dfa06bd
Jon H <johnnybhendon@gmail.com>
2024-05-29 11:33:34 -0400

    Return an average from an array

---
+++ 8kyu/avgOfNums.js
@@ -0,0 +1,11 @@
+/*
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+
+//Answer
+
+function findAverage(array){
+    return array.length === 0 ? 0 : array.reduce((acc, c)=> acc + c, 0) / array.length
+}
